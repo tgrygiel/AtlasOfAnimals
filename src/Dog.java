56aedf880ea9677abb73d38440cd6c90147b263b
OlekKhom <olekkhomenko90@gmail.com>
2021-02-20 13:30:51 +0100

    addKonstruktorGrtSet

--- src/Dog.java
+++ src/Dog.java
@@ -1,6 +1,43 @@
 
 public class Dog {
-	String name;
-	int age;
-	String type;
+	private String name;
+	private int age;
+	private String type;
+
+
+	public Dog(String name, int age, String type ) {
+		this.name = name;
+		this.age = age;
+		this.type = type;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Dog setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Dog setAge(int age) {
+		this.age = age;
+		return this;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public Dog setType(String type) {
+		this.type = type;
+		return this;
+	}
+
+
 }
